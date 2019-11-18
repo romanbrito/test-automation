de3e396043de1000e93918c5be0978e7ac054b5d
Roman Brito <roman.brito@osmsolutions.com>
2019-11-18 15:04:13 -0600

    dq user can upload media

--- test/specs/dqSpec.js
+++ test/specs/dqSpec.js
@@ -6,19 +6,15 @@ describe("Smoke Test", () => {
         $("#user_login").waitForEnabled(3000);
         $("#user_pass").waitForEnabled(3000);
         $("#wp-submit").waitForEnabled(3000);
-        // $("#comments").waitForEnabled(3000);
 
         $("#user_login").setValue( userName );
         $("#user_pass").setValue( password );
-        // $("#state").selectByVisibleText("Texas");
-
-        // var backpack = $("#backpack");
+        
         var submit_button = $("#wp-submit");
         browser.execute("arguments[0].scrollIntoView();", submit_button);
         submit_button.click();
 
         browser.url( serverUrl );
-
 
         $("//button[text()='Create New Sign']").waitForEnabled(10000);
         var create_sign_button = $("//button[text()='Create New Sign']");
@@ -27,31 +23,23 @@ describe("Smoke Test", () => {
         $(".modal-content").waitForEnabled(3000);
 
         $("#newSignName").waitForEnabled(3000);
-        var signName = "test";
+        var signName = "Test/smoke" + Date.now();
         $("#newSignName").setValue( signName );
 
         $("#newPresentationName").waitForEnabled(10000);
-        var presentationName = "first"
+        var presentationName = "allDay"
         $("#newPresentationName").setValue( presentationName );
-
-        // var element = $("//button[text()='Create']");
 
         $("#createSignModal .modal-footer button.btn.btn-primary").waitForEnabled(10000);
         var create = $("#createSignModal .modal-footer button.btn.btn-primary");
         create.click();
 
-        // $("#fileupload").waitForEnabled(3000);
-        // $("#fileupload").setValue("~/Workspace/test-automation/media/BlackBoardLandscape.jpeg");
+        $("#fileupload").waitForEnabled(5000);
+        $("#fileupload").setValue("/Users/romanbrito/Workspace/test-automation/media/BlackBoardLandscape.jpeg");
 
-        // $("button:contains('Create')").waitForEnabled(3000);
-        // $("button:contains('Create')").click();
+        $("//div[text()='Upload finished.']").waitForEnabled(10000);
+        $("#message").getText().should.equal("Upload finished.");
 
-        // $("#newsletter_yes").click();
-
-        // $("#user_login").getValue().should.equal("Roman");
-        // $("#state").getValue().should.equal("TX");
-        // backpack.isSelected().should.equal(true);
-        // $("#newsletter_yes").isSelected().should.equal(true);
     });
 });
 
